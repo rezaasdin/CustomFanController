464455a95d5cea52780db3ca1ec427f836130b2d
Reza Asadian <rezaasadian100@gmail.com>
2021-08-27 15:53:40 +0430

    A special color for each degree of speed

--- app/src/main/java/com/example/customfancontroller/DialView.kt
+++ app/src/main/java/com/example/customfancontroller/DialView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.math.cos
 import kotlin.math.min
 import kotlin.math.sin
@@ -20,11 +21,11 @@ private enum class FanSpeed(val label: Int) {
         MEDIUM -> HIGH
         HIGH -> OFF
     }
+
 }
 
 private const val RADIUS_OFFSET_LABEL = 30
 private const val RADIUS_OFFSET_INDICATOR = -35
-
 
 class DialView @JvmOverloads constructor(
     context: Context,
@@ -32,11 +33,11 @@ class DialView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+
     private var radius = 0.0f
     private var fanSpeed = FanSpeed.OFF
 
     private val pointPosition: PointF = PointF(0.0f, 0.0f)
-
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
@@ -45,20 +46,28 @@ class DialView @JvmOverloads constructor(
         typeface = Typeface.create("", Typeface.BOLD)
     }
 
+    private var fanSpeedLowColor = 0
+    private var fanSpeedMediumColor = 0
+    private var fanSeedMaxColor = 0
 
-    init{
+    init {
         isClickable = true
+
+        context.withStyledAttributes(attrs, R.styleable.DialView) {
+            fanSpeedLowColor = getColor(R.styleable.DialView_fanColor1, 0)
+            fanSpeedMediumColor = getColor(R.styleable.DialView_fanColor2, 0)
+            fanSeedMaxColor = getColor(R.styleable.DialView_fanColor3, 0)
+        }
     }
 
     override fun performClick(): Boolean {
-        if(super.performClick()) return true
+        if (super.performClick()) return true
 
         fanSpeed = fanSpeed.next()
         contentDescription = resources.getString(fanSpeed.label)
 
         invalidate()
         return true
-
     }
 
 
@@ -75,7 +84,14 @@ class DialView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+
+        paint.color = when (fanSpeed) {
+            FanSpeed.OFF -> Color.GRAY
+            FanSpeed.LOW -> fanSpeedLowColor
+            FanSpeed.MEDIUM -> fanSpeedMediumColor
+            FanSpeed.HIGH -> fanSeedMaxColor
+        } as Int
+
         canvas?.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
 
         val markerRadius = radius + RADIUS_OFFSET_INDICATOR
@@ -83,16 +99,14 @@ class DialView @JvmOverloads constructor(
         paint.color = Color.BLACK
         canvas?.drawCircle(pointPosition.x, pointPosition.y, radius / 12, paint)
 
+
         val labelRadius = radius + RADIUS_OFFSET_LABEL
         for (i in FanSpeed.values()) {
-
             pointPosition.computeXYForSpeed(i, labelRadius)
             val label = resources.getString(i.label)
             canvas?.drawText(label, pointPosition.x, pointPosition.y, paint)
-
         }
 
     }
-
 
 }
